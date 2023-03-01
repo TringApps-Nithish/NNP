c1961ac4e1899c0809dd02c64608d82b15901b12
VijayBalaji06 <nvbalaji6@gmail.com>
2023-03-01 23:36:44 +0530

    new

---
+++ src/main/java/org/example/Nnp.java
@@ -0,0 +1,12 @@
+package org.example;
+
+import java.util.logging.Logger;
+
+public class Nnp {
+    public static void main(String[] args) {
+
+        Logger l=Logger.getLogger("Nnp");
+        l.info("Hello");
+        l.info("Npp");
+    }
+}
